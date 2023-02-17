b5c0da85a5e8bf5dc22391493a7f8d07bdf1aab8
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2023-02-17 08:58:56 +0000

    find lines with no bananas

--- Data_Manipulation/filter_lines.sh
+++ Data_Manipulation/filter_lines.sh
@@ -3,4 +3,19 @@ grep apple rfruits.txt > apples.txt
 # count the word 'apple'
 napples=`grep -c apple apples.txt`
 
-echo $'\nThere are {napples} here.' >> apples.txt
+echo -e "\nThere are $napples here." >> apples.txt
+
+# find lines that contain either 'banana' or 'orange'
+grep -e banana -e orange rfruits.txt > banorg.txt
+# count them
+nbanana=`grep -c banana rfruits.txt`
+norange=`grep -c orange rfruits.txt`
+nbanorg=`grep -c -e banana -e orange rfruits.txt`
+
+echo -e "\nThere are $nbanana bananas\n$norange oranges\nTotal is $nbanorg." >> banorg.txt
+
+# find all lines that don't contail banana
+grep -v banana rfruits.txt > nobanana.txt
+# count them 
+noban=`grep -vc banana rfruits.txt`
+echo -e "\nTotal are $noban." >> nobanana.txt
